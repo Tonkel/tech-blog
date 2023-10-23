d08dca10b3064a4352bab26ffbf4d77d4ed2f6d8
Tonkel <w.tucker.cac@gmail.com>
2023-10-23 17:36:16 -0400

    set up model relations

--- models/index.js
+++ models/index.js
@@ -1,3 +1,32 @@
-const User = require('./User');
+const User = require("./User");
+const Post = require("./posts");
+const Comment = require("./comments");
+
+User.hasMany(Post, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+  onDelete: "CASCADE",
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+});
+
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+});
 
 module.exports = { User };
